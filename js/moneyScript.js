f38f5993924db4e7f4a62917998613ef38677843
VACBANe <volmaster25rus@gmail.com>
2021-04-25 20:14:59 +0300

    moneyconvNewFile

--- js/moneyScript.js
+++ js/moneyScript.js
@@ -1,13 +1,17 @@
-let sellUahFromUsd = 28
-let sellUahFromRur = 0.38
+let sellUahUsd = 28
+let sellUahEur = 33
+let sellUahRur = 0.38
+let sellUahBtc = null
 const takeData = () => {
     fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            sellUahFromUsd = data[0]["sale"]
-            sellUahFromRur = data[2]["sale"]
+            sellUahUsd = data[0]["sale"]
+            sellUahEur = data[1]["sale"]
+            sellUahRur = data[2]["sale"]
+            sellUahBtc = data[3]["sale"]
             console.log("Successful")
         })
         .catch(function (error) {
@@ -16,5 +20,8 @@ const takeData = () => {
 }
 takeData()
 const convertUahIntoUsd = (value) => {
-    document.getElementById("uahSellUsdOutput").innerHTML = (value / sellUahFromUsd).toFixed(2).toString()
+    document.getElementById("uahSellUsdOutput").innerHTML = (value / sellUahUsd).toFixed(2).toString()
+    document.getElementById("uahSellEurOutput").innerHTML = (value / sellUahEur).toFixed(2).toString()
+    document.getElementById("uahSellRurOutput").innerHTML = (value / sellUahRur).toFixed(2).toString()
+    document.getElementById("uahSellBtcOutput").innerHTML = (value / sellUahBtc).toFixed(6).toString()
 }

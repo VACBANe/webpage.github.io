92ff36c93f7600b2095d6b97effc47bc86e71312
VACBANe <volmaster25rus@gmail.com>
2021-04-28 20:32:12 +0300

    rockPaperScissors

--- js/rpc.js
+++ js/rpc.js
@@ -1,36 +1,90 @@
+const rock = document.getElementById("rock");       //0 - rock
+const paper = document.getElementById("paper");     //1 - paper
+const scissors = document.getElementById("scissors");//2 - scissors
+const your = document.getElementById("yourImg");
+const bot = document.getElementById("botImg");
+const yourScoreText = document.getElementById("yourScoreText");
+const botScoreText = document.getElementById("botScoreText");
 let botValue = null;
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-const your = document.getElementById("you");
-const bot = document.getElementById("bot");
-
-//0 - rock
-//1 - paper
-//2 - scissors
-
-
-
+let yourScore = 0;
+let botScore = 0;
 function play (value) {
+    if(yourScore === 5 || botScore === 5) {
+        return 0;
+    }
     botValue = Math.floor(Math.random() * 3);
-    console.log(botValue)
+    (value === 0)? your.src = 'images/rock.png'
+        : value === 1? your.src = 'images/paper.png'
+        : your.src = 'images/scissors.png';
+    (botValue === 0)? bot.src = 'images/rock.png'
+        : botValue === 1? bot.src = 'images/paper.png'
+        : bot.src = 'images/scissors.png';
+    console.log(botValue);
     switch(value){
         case 0:
-            return botValue == 1? "You loose": (botValue == 2)?"You won" : "Draws"
+            if(botValue === 1) {
+                botScore++;
+            } else if(botValue === 2) {
+                yourScore++;
+            }
+            break;
         case 1:
-            return botValue == 2? "You loose": (botValue == 0)?"You won" : "Draws"
+            if(botValue === 2) {
+                botScore++;
+            } else if(botValue === 0) {
+                yourScore++;
+            }
+            break;
         case 2:
-            return botValue == 0? "You loose": (botValue == 1)?"You won" : "Draws"
+            if(botValue === 0) {
+                botScore++;
+            } else if(botValue === 1) {
+                yourScore++;
+            }
+            break;
+    }
+    changeScore(yourScore, botScore);
+    if(yourScore === 5 || botScore === 5) {
+        endGame(yourScore);
     }
 }
 
 rock.onclick = () => {
-    console.log(play(0))
-    bot.href = "url('https://static.thenounproject.com/png/88666-200.png')";
+    play(0);
 }
 paper.onclick = () => {
-    console.log(play(1))
+    play(1);
 }
 scissors.onclick = () => {
-    console.log(play(2))
+    play(2);
 }
+
+const changeScore = (a,b) => {
+    yourScoreText.textContent = a.toString();
+    botScoreText.textContent = b.toString();
+}
+
+// HUD
+const startbtn = document.getElementById("start_btn")
+const playbtns = document.getElementById("play_buttons")
+const restartbtn = document.getElementById("restart_btn")
+const result = document.getElementById("res");
+
+startbtn.onclick = () => {
+    startbtn.style.display = "none";
+    playbtns.style.display = "block";
+}
+
+restartbtn.onclick = () => {
+    yourScore = botScore = 0;
+    bot.src = your.src = result.textContent = "";
+    restartbtn.style.display = "none";
+    changeScore(yourScore, botScore);
+}
+
+const endGame = (d) => {
+    d === 5 ? result.textContent = "You won! Congratulations!"
+        : result.textContent = "Fail...";
+    restartbtn.style.display = "block";
+}
+
